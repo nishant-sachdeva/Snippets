bc21d7229f8f644eb0ecf6c1c61471445e37e3d8
nishant-sachdeva <nishant.sachdeva@research.iiit.ac.in>
2019-09-22 10:00:41 +0530

    changes

---
+++ base.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long int
+#define get(a) ll a; cin >> a;
+#define show(a) cout << a << endl;
+#define full(p) p.begin(), p.end()
+#define sz(a) ll(a.size())
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.precision(10);
+	cout<<fixed;
+	////////////////////////////////
+	
+
+	return 0;
+}
